9570a6ad81ead40f9aee105fa119b27df4643ccd
kaoutar afkhar <kaoutarafkhar@192.168.11.142>
2025-11-16 23:22:48 +0100

    edit

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,13 +4,12 @@ function MainContent() {
       style={{
         padding: '20px',
         backgroundColor: '#eef2f3',
-        minHeight: '150px'
+        minHeight: '150px',
+        textAlign: 'center'
       }}
     >
-      <h2 style={{ textAlign: 'center' }}>Welcome to the App</h2>
-      <p style={{ marginTop: '10px' }}>
-        This is the main content area.
-      </p>
+      <h2>Welcome to the App</h2>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
