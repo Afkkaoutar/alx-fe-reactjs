90d3598cb4765981ab5136ae16f9492b1bc9f846
kaoutar afkhar <kaoutarafkhar@192.168.11.142>
2025-11-16 23:33:27 +0100

    Fix Counter import for checker

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,20 +2,19 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
 import Footer from "./components/Footer";
+import Counter from "./components/Counter"; 
 
 function App() {
   return (
     <>
       <Header />
-
       <MainContent />
-
       <UserProfile
         name="John Doe"
         age={25}
         bio="A traveler who loves exploring new cities."
       />
-
+      <Counter /> {}
       <Footer />
     </>
   );
